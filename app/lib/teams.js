8f7e3ea2ef57e387c5f4a39f6ad3039d98265ad2
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-24 15:25:18 +0100

    Created methods to find all leagues and specific league

--- app/lib/teams.js
+++ app/lib/teams.js
@@ -1,20 +1,34 @@
+var request = require('request');
+
 var Teams = function(league) {
   this.league = league;
 };
 
 Teams.prototype.all = function(callback) {
-  var leagueURL = this._league(premierLeagueJSON._links.teams.href);
-  this._request(leagueLink, function(res) { 
-    
+  var leagueURL = this.league._links.teams.href;
+  request.get(leagueURL, function(err, res, body) { 
+    callback(err, body);
   });
 };
 
-Teams.prototype._league = function() {
-  return this.league;
+Teams.prototype.find = function(team, callback) {
+  var that = this;
+  this.all(function(err, teams) { 
+    var teamJSON = that._findTeam(JSON.parse(teams), team);
+    callback(err, teamJSON);
+  });
 };
 
-Teams.prototype._request = function(url, callback) {
-  http.get(url, callback);
+Teams.prototype._findTeam = function(teams, team) {
+  for(var i = 0; i < teams.teams.length; i++) { 
+    var teamNameJSON = teams.teams[i].code;
+    var regEx = new RegExp(team, 'g');
+    var match = teamNameJSON.match(regEx);
+    if(match) { 
+      return teams.teams[i];
+    };
+  }
 };
+
 
 module.exports = Teams;
