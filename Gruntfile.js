e81183851c8df92b64b963e9e97c1abd0bfaf78d
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-26 15:14:31 +0100

    Updated Gruntfile to make testing easier

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     express: {
       default_option: {}
+    }, 
+    mochacli: {
+      options: {
+        require: ['chai'],
+        reporter: 'spec',
+        timeout: 100000,
+        bail: true
+      },
+      all: ['test/**/*.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-express');
+  grunt.loadNpmTasks('grunt-mocha-cli');
 
   grunt.registerTask('default', ['express']);
-
+  grunt.registerTask('test', ['mochacli']);
 };
