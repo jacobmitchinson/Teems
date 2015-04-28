9ce555ebc8456e45b72288d01af6ee0306711f3e
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-28 11:19:36 +0100

    Made adjustments to server ready for integration with mongo

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var app = require('express')();
 var http = require('http');
+var config = require('./config');
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.use('/js', express.static(__dirname + '/public/js'))
 app.use('/css', express.static(__dirname + '/public/css'));
+
+app.configure(function() { 
+  app.set('dbUrl', config.db[app.settings.env]);
+  mongoose.connect(app.get('dbUrl'));
+});
 
 app.get('/', function(req, res) {
   res.render('index');
