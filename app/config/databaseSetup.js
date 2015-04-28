0d122143b155210f147efaff6d08cd615d8f00bc
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-28 11:11:36 +0100

    Created config for database

---
+++ app/config/databaseSetup.js
@@ -0,0 +1,7 @@
+module.exports = { 
+  db: { 
+    production: "mongodb://localhost:27017/teemsproduction",
+    development: "mongodb://localhost:27017/teemsdevelopment",
+    test: "mongodb://localhost:27017/teemstest"
+  }
+};
