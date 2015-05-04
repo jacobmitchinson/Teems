fc479a38d5bbe6f80e5fab4ab2a921bd4289b937
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-04 17:01:59 +0100

    Implemented player schema

--- app/models/player.js
+++ app/models/player.js
@@ -3,8 +3,23 @@ var Schema = mongoose.Schema;
 
 var playerSchema = new Schema({ 
   name: String,
+  team: String,
+  position: String,
+  jerseyNumber: Number,
+  dateOfBirth: String,
+  contractUntil: String,
+  nationality: String,
+  location: String,
+  marketValue: Number,
+  twitterUrl: String,
   screenName: String,
-  twitterID: String 
+  twitterID: Number,
+  followersCount: Number,
+  friendCount: Number 
 });
+
+// playerSchema.prototype.convertDOB = function(DOB) {
+//   new Date('2011-04-11')
+// };
 
 exports.Player = mongoose.model('Player', playerSchema);
